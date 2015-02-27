bdd873304255407b66f9c3f78d8a867c8ca03655
Pier Fumagalli <pier@usrz.com>
2015-02-28 03:11:00 +0900

    EC to Lombok without builders.

--- source/main/org/usrz/jose/jwk/ec/ECJWK.java
+++ source/main/org/usrz/jose/jwk/ec/ECJWK.java
@@ -16,26 +16,45 @@
 package org.usrz.jose.jwk.ec;
 
 import java.math.BigInteger;
-import java.security.interfaces.ECPrivateKey;
+import java.security.Key;
+import java.security.interfaces.ECKey;
 
 import org.usrz.jose.jwk.JWK;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * Implementation of the {@link JWK} interface for Elliptic Curve Private Keys.
+ * An abstract implementation of the {@link JWK} interface for Elliptic Curves.
  */
-public interface ECPrivateJWK
-extends ECJWK<ECPrivateKey> {
+public interface ECJWK<KEY extends Key & ECKey>
+extends JWK<KEY> {
 
-    /** The {@code d} JWK <i>("{@code EC}")</i> field name. */
-    public static final String ECC_PRIVATE_KEY = "d";
+    /** The {@code crv} JWK <i>("{@code EC}")</i> field name. */
+    public static final String CURVE = "crv";
+    /** The {@code x} JWK <i>("{@code EC}")</i> field name. */
+    public static final String X_COORDINATE = "x";
+    /** The {@code y} JWK <i>("{@code EC}")</i> field name. */
+    public static final String Y_COORDINATE = "y";
 
     /**
-     * The "d" (ECC private key) member contains the Elliptic Curve private
-     * key value.
+     * The "crv" (curve) member identifies the cryptographic curve used with
+     * the key.
      */
-    @JsonProperty(ECC_PRIVATE_KEY)
-    public BigInteger getECCPrivateKey();
+    @JsonProperty(CURVE)
+    public ECCurve getCurve();
+
+    /**
+     * The "x" (x coordinate) member contains the x coordinate for the
+     * elliptic curve point.
+     */
+    @JsonProperty(X_COORDINATE)
+    public BigInteger getXCoordinate();
+
+    /**
+     * The "y" (y coordinate) member contains the y coordinate for the
+     * elliptic curve point.
+     */
+    @JsonProperty(Y_COORDINATE)
+    public BigInteger getYCoordinate();
 
 }
--- source/main/org/usrz/jose/jwk/ec/ECPublicJWK.java
+++ source/main/org/usrz/jose/jwk/ec/ECPublicJWK.java
@@ -15,72 +15,16 @@
  * ========================================================================== */
 package org.usrz.jose.jwk.ec;
 
-import java.math.BigInteger;
-import java.net.URI;
-import java.security.cert.X509Certificate;
 import java.security.interfaces.ECPublicKey;
-import java.util.List;
 
-import org.usrz.jose.JOSEAlgorithm;
 import org.usrz.jose.jwk.JWK;
-import org.usrz.jose.jwk.JWKKeyOperation;
-import org.usrz.jose.jwk.JWKKeyType;
-import org.usrz.jose.jwk.JWKPublicKeyUse;
-import org.usrz.jose.jwk.PublicJWK;
 
 /**
  * Implementation of the {@link JWK} interface for Elliptic Curve Public Keys.
  */
-public class ECPublicJWK
-extends ECAbstractJWK<ECPublicKey>
-implements PublicJWK<ECPublicKey> {
+public interface ECPublicJWK
+extends ECJWK<ECPublicKey> {
 
-    protected ECPublicJWK(JOSEAlgorithm algorithm,
-                          String keyID,
-                          URI x509url,
-                          List<X509Certificate> x509CertificateChain,
-                          byte[] x509CertificateThumbprint,
-                          byte[] x509CertificateThumbprintSHA256,
-                          JWKKeyType keyType,
-                          JWKPublicKeyUse publicKeyUse,
-                          List<JWKKeyOperation> keyOperations,
-                          ECCurve curve,
-                          BigInteger x,
-                          BigInteger y) {
-        super(algorithm,
-              keyID,
-              x509url,
-              x509CertificateChain,
-              x509CertificateThumbprint,
-              x509CertificateThumbprintSHA256,
-              keyType,
-              publicKeyUse,
-              keyOperations,
-              curve,
-              x,
-              y);
-    }
+    /* JWSHeader is a simple marker interface */
 
-    /**
-     * A builder to construct {@link ECPublicJWK} instances.
-     */
-    public static class Builder
-    extends ECAbstractJWK.Builder<ECPublicKey, ECPublicJWK, ECPublicJWK.Builder> {
-
-        @Override
-        public ECPublicJWK build() {
-            return new ECPublicJWK(algorithm,
-                                   keyId,
-                                   x509Url,
-                                   x509CertificateChain,
-                                   x509CertificateThumbprint,
-                                   x509CertificateThumbprintSHA256,
-                                   keyType,
-                                   publicKeyUse,
-                                   keyOperations,
-                                   curve,
-                                   x,
-                                   y);
-        }
-    }
 }
