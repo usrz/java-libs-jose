c8ee5ee3844cf56849190a1168fe26a04c1122b0
Pier Fumagalli <pier@usrz.com>
2015-02-28 02:22:38 +0900

    Lomboked JWE.

--- source/main/org/usrz/jose/jwe/JWEHeader.java
+++ source/main/org/usrz/jose/jwe/JWEHeader.java
@@ -22,108 +22,85 @@ import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 
+import lombok.Data;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import org.usrz.jose.core.BeanBuilder;
 import org.usrz.jose.core.Bytes;
-import org.usrz.jose.impl.AbstractJOSEHeader;
+import org.usrz.jose.core.Header;
 import org.usrz.jose.jwk.JWK;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
-public class JWEHeader extends AbstractJOSEHeader<JWEAlgorithm> {
+@JsonDeserialize(builder=JWEHeader.Builder.class)
+public interface JWEHeader extends Header<JWEAlgorithm> {
 
-    private final JWEEncryption encryption;
-    private final JWECompression compression;
-
-    protected JWEHeader(final JWEAlgorithm algorithm,
-                        final String keyID,
-                        final URI x509URI,
-                        final List<X509Certificate> x509CertificateChain,
-                        final Bytes x509CertificateThumbprint,
-                        final Bytes x509CertificateThumbprintSHA256,
-                        final URI jwkSetURL,
-                        final JWK<?> jwk,
-                        final MediaType type,
-                        final MediaType contentType,
-                        final List<String> criticalExtensions,
-                        final Map<String, Object> additionalHeaders,
-                        final JWEEncryption encryption,
-                        final JWECompression compression) {
-        super(algorithm,
-              keyID,
-              x509URI,
-              x509CertificateChain,
-              x509CertificateThumbprint,
-              x509CertificateThumbprintSHA256,
-              jwkSetURL,
-              jwk,
-              type,
-              contentType,
-              criticalExtensions,
-              additionalHeaders);
-        this.encryption = encryption;
-        this.compression = compression;
-    }
+    /** The {@code enc} header field name. */
+    public static final String ENCRYPTION = "enc";
+    /** The {@code zip} header field name. */
+    public static final String COMPRESSION = "zip";
 
     /**
      * The "enc" (encryption algorithm) Header Parameter identifies the
      * content encryption algorithm used to perform authenticated encryption
      * on the Plaintext to produce the Ciphertext and the Authentication Tag.
      */
-    @JsonProperty("enc")
-    public JWEEncryption getEncryption() {
-        return encryption;
-    }
+    @JsonProperty(ENCRYPTION)
+    public JWEEncryption getEncryption();
 
     /**
      * The "zip" (compression algorithm) applied to the Plaintext before
      * encryption, if any.
      */
-    @JsonProperty("zip")
-    public JWECompression getCompression() {
-        return compression;
-    }
+    @JsonProperty(ENCRYPTION)
+    public JWECompression getCompression();
 
-    public static class Builder extends AbstractJOSEHeader.Builder<JWEAlgorithm, JWEHeader, Builder> {
+    @Accessors(chain=true)
+    @JsonPOJOBuilder(withPrefix="set")
+    public static final class Builder
+    extends Header.Builder<JWEAlgorithm, JWEHeader, Builder> {
 
-        protected JWEEncryption encryption;
-        protected JWECompression compression;
-
-        @Override
-        public JWEHeader build() {
-            return new JWEHeader(algorithm,
-                                 keyId,
-                                 x509Url,
-                                 x509CertificateChain,
-                                 x509CertificateThumbprint,
-                                 x509CertificateThumbprintSHA256,
-                                 jsonWebKeySetUrl,
-                                 jsonWebKey,
-                                 mediaType,
-                                 contentMediaType,
-                                 criticalExtensions,
-                                 additionalHeaders,
-                                 encryption,
-                                 compression);
-        }
+        private static final BeanBuilder<Builder, Impl> BUILDER = new BeanBuilder<>(Builder.class, Impl.class);
 
         /**
          * The "enc" (encryption algorithm) Header Parameter identifies the
          * content encryption algorithm used to perform authenticated encryption
          * on the Plaintext to produce the Ciphertext and the Authentication Tag.
          */
-        @JsonProperty("enc")
-        public Builder withEncryption(JWEEncryption encryption) {
-            this.encryption = encryption;
-            return builder;
-        }
+        @Setter(onMethod=@__({@JsonProperty(ENCRYPTION)}))
+        private JWEEncryption encryption;
 
         /**
          * The "zip" (compression algorithm) applied to the Plaintext before
          * encryption, if any.
          */
-        @JsonProperty("zip")
-        public Builder withCompression(JWECompression compression) {
-            this.compression = compression;
-            return builder;
+        @Setter(onMethod=@__({@JsonProperty(COMPRESSION)}))
+        private JWECompression compression;
+
+        @Override
+        public JWEHeader build() {
+            return BUILDER.build(this);
+        }
+
+        @Data
+        private static final class Impl implements JWEHeader {
+            private final JWEAlgorithm algorithm;
+            private final String keyId;
+            private final URI x509Url;
+            private final List<X509Certificate> x509CertificateChain;
+            private final Bytes x509CertificateThumbprint;
+            private final Bytes x509CertificateThumbprintSHA256;
+            private final URI jsonWebKeySetUrl;
+            private final JWK<?> jsonWebKey;
+            private final MediaType mediaType;
+            private final MediaType contentMediaType;
+            private final List<String> criticalExtensions;
+            private final Map<String, Object> additionalHeaders;
+            private final JWEEncryption encryption;
+            private final JWECompression compression;
         }
     }
 }
