96961ab0ec13176659e4aee695a81c71f9f678fb
Pier Fumagalli <pier@usrz.com>
2015-02-28 20:53:15 +0900

    JWKSet implementation.

--- source/main/org/usrz/jose/jwk/JWKSet.java
+++ source/main/org/usrz/jose/jwk/JWKSet.java
@@ -15,17 +15,86 @@
  * ========================================================================== */
 package org.usrz.jose.jwk;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import org.usrz.jose.core.BeanBuilder;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 /**
  * A JSON Web Key Set (JWK Set) is a JSON object that represents a set
  * of JWKs.
  */
+@JsonDeserialize(builder=JWKSet.Builder.class)
 public interface JWKSet {
+
+    /** The {@code keys} JWK set field name. */
+    public static final String KEYS = "keys";
 
     /**
      * The value of the "keys" member is an array of {@link JWK} values.
      */
+    @JsonProperty(KEYS)
     public List<JWK<?>> getKeys();
 
+    /* ====================================================================== */
+
+    @Accessors(chain=true)
+    @JsonPOJOBuilder(withPrefix="set")
+    public class Builder
+    extends BeanBuilder<JWKSet> {
+
+        @SuppressWarnings("unused")
+        private final List<JWK<?>> keys;
+        private final List<JWK<?>> keysList;
+
+        @SuppressWarnings("unchecked")
+        public Builder() {
+            super(Impl.class);
+
+            keysList = new ArrayList<>();
+            keys = Collections.unmodifiableList(keysList);
+        }
+
+        @Override
+        public JWKSet build() {
+            return super.build();
+        }
+
+        /**
+         * The value of the "keys" member is an array of {@link JWK} values.
+         */
+        @JsonProperty(KEYS)
+        public Builder setKeys(List<JWK<?>> keys) {
+            if (keys == null) return this;
+            keys.forEach((criticalExtension) -> {
+                keysList.add(criticalExtension);
+            });
+            return this;
+        }
+
+        /**
+         * The value of the "keys" member is an array of {@link JWK} values.
+         */
+        @JsonIgnore
+        public Builder addKey(JWK<?> key) {
+            keysList.add(key);
+            return this;
+        }
+
+        /* ================================================================== */
+
+        @Data
+        private static final class Impl implements JWKSet {
+            private final List<JWK<?>> keys;
+        }
+    }
 }
