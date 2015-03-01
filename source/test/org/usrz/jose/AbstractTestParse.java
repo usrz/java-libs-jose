c3777f272640e18b9832e55ce13973451143037e
Pier Fumagalli <pier@usrz.com>
2015-03-01 23:53:20 +0900

    Parse numbers and byte arrays.

--- source/test/org/usrz/jose/AbstractTestParse.java
+++ source/test/org/usrz/jose/AbstractTestParse.java
@@ -1,9 +1,12 @@
 package org.usrz.jose;
 
+import java.math.BigInteger;
 import java.net.URL;
 
+import org.usrz.jose.core.Bytes;
 import org.usrz.jose.jackson.JOSEObjectMapper;
 import org.usrz.libs.testing.AbstractTest;
+import org.usrz.libs.utils.codecs.Base64Codec;
 
 import com.fasterxml.jackson.core.PrettyPrinter;
 import com.fasterxml.jackson.core.TreeNode;
@@ -13,6 +16,7 @@ public class AbstractTestParse extends AbstractTest {
 
     protected final JOSEObjectMapper mapper = new JOSEObjectMapper();
     protected final PrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+    protected final Base64Codec base64 = new Base64Codec(Base64Codec.Alphabet.URL_SAFE);
 
     protected URL getResource(String name) {
         final URL url = this.getClass().getResource(name);
@@ -35,5 +39,11 @@ public class AbstractTestParse extends AbstractTest {
         }
     }
 
+    protected Bytes parseBytes(String bytes) {
+        return new Bytes(base64.decode(bytes));
+    }
 
+    protected BigInteger parseBigInteger(String bigInteger) {
+        return new BigInteger(1, base64.decode(bigInteger));
+    }
 }
